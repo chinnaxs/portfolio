15b0e96eb0489f554f17cf67b9c77fb5fa57b1ef
chinnaxs <mathiaschinna@gmail.com>
2022-07-26 22:01:53 +0200

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Component } from "react";
+import { Typography } from "@material-ui/core";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Typography variant="h1">Hello World</Typography>
+      </div>
+    );
+  }
+}

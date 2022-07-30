66edc43fc4732ecbf6e0c895df7867cb94146326
chinnaxs <mathiaschinna@gmail.com>
2022-07-30 20:28:34 +0200

    added textzone

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 
 import SkillBarList from "./skill-bar-list/SkillBarList";
+import TextZone from "./text-zone/TextZone";
 
 
 export default class About extends Component {
 
 
   render() {
-    return <div><SkillBarList /> </div>
+    return <div className="flex flex-row grid grid-cols-3 gap-4 p-4">
+      <div className="col-span-1"><TextZone/></div>
+      <div className="col-span-2"><SkillBarList /> </div>
+      </div>
   }
 }

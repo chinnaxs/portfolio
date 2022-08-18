15c1a20959f9b88a2cb1e98c8514a0e7a9ee743f
chinnaxs <mathiaschinna@gmail.com>
2022-08-18 22:23:49 +0200

    made welcome section responsive

--- src/components/main/welcome/Welcome.tsx
+++ src/components/main/welcome/Welcome.tsx
@@ -10,24 +10,23 @@ export default class Welcome extends Component {
     // TODO: figure out how to insert flag at once
     return (
       <Fade>
-        <div className="max-w-4xl mx-auto pt-[50px] bg-deep-blue h-screen flex items-center justify-center">
-          {/* <div className="mx-auto bg-grayd" */}
-          <div className="flex flex-nowrap my-auto mx-auto bg-white w-2/3 rounded-xl">
+        <div className="container max-w-4xl mx-auto pt-4 h-screen flex items-center justify-center">
+          <div className="flex flex-col md:flex-row my-auto mx-auto bg-white w-[250px] md:w-2/3 rounded-xl">
             <img
-              className="inline-block rounded-l-xl"
+              className="rounded-t-xl md:rounded-tr-none md:rounded-l-xl"
               src="/assets/me_square.jpg"
               style={{ width: 250, height: 250 }}
               alt=""
               loading="lazy"
             />
-            <div className="inline-block self-center px-4 text-black text-xl ">
+            <div className="self-center px-4 text-black text-xl my-4 md:mt-0">
               <p className="font-bold">Hi, I'm Matthias Lei</p>
 
               <TypeAnimation
                 cursor={true}
                 sequence={[500, "full-stack dev from 🇨🇭"]}
                 wrapper="p"
-                className="font-monospace"
+                className="font-monospace overflow-hidden"
               />
 
             </div>
